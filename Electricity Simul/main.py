e15652526d68200d2dbb1c9850355f37519c9215
Luka <lukakarathanos@gmail.com>
2022-11-15 13:52:33 +0000

    First Primitive Graph

--- Electricity Simul/main.py
+++ Electricity Simul/main.py
@@ -9,6 +9,7 @@ import mesa
 import elec_market
 import world_model 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
 # class ElecModel(mesa.Model):
 #     def __init__(self):
 #         elec_co = ElecCo('edf', data.list_of_plants)
@@ -17,7 +18,38 @@ import matplotlib.pyplot as plt
 #     def step(self):
 #         """Advance the model by one step."""
 #         self.schedule.step()
-        
+
+
+fsize = 11
+tsize = 18
+tdir = 'in'
+major = 5.0
+minor = 3.0
+lwidth = 0.8
+lhandle = 2.0
+plt.style.use('default')
+plt.rcParams['font.family'] = 'Times New Roman'
+plt.rcParams['font.size'] = fsize
+plt.rcParams['legend.fontsize'] = tsize
+plt.rcParams['xtick.direction'] = tdir
+plt.rcParams['ytick.direction'] = tdir
+plt.rcParams['xtick.major.size'] = major
+plt.rcParams['xtick.minor.size'] = minor
+plt.rcParams['ytick.major.size'] = 5.0
+plt.rcParams['ytick.minor.size'] = 3.0
+plt.rcParams['axes.linewidth'] = lwidth
+plt.rcParams['legend.handlelength'] = lhandle
+plt.rcParams['axes.autolimit_mode'] = 'round_numbers'
+plt.rcParams['axes.xmargin'] = 0
+plt.rcParams['axes.ymargin'] = 0
+
+
+
+
+
+
+
+
 
 def run(n_years, n_days):
     '''
@@ -51,11 +83,20 @@ def run(n_years, n_days):
         model.current_year += 1
 
 
-    print(average_yearly_strike_prices)
-
+    #The plot -> configured properly
+    
     fig, ax = plt.subplots()
     ax.plot(range(n_years), average_yearly_strike_prices)
+    ax.set_ylabel('Average price per MWh (£)')
+    ax.set_xlabel('Year')
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+    ax.yaxis.set_minor_locator(AutoMinorLocator())
+    # ax.set_xlim(0, n_years)
+    # ax.set_ylim(min(average_yearly_strike_prices) - 5, (max(average_yearly_strike_prices) + 5))
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
     plt.show
+    print(average_yearly_strike_prices)
 
 #number of representative days within the year.
 n_days = 4
@@ -65,5 +106,7 @@ n_years = 30
 if __name__ == '__main__':
 
     run(n_years,n_days)
+
+
 
 # %%
